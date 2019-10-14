db39886f0b25f40b6c4f6479048b9f88e0ea3c1e
Simon <simonenterprise94@gmail.com>
2019-10-14 15:19:12 +0300

    feat: add project item component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import ProjectItems from './Project/ProjectItems';
 
 const Dashboard = () => (
-  <h1> Welcome to the dashboard </h1>
+  <div>
+    <h1> Welcome to the dashboard </h1>
+    <ProjectItems />
+  </div>
 );
+
+
 export default Dashboard;
