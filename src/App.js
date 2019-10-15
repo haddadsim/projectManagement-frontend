dcc8d9e8447df7d45babf6b721e05a80952b8d0a
Simon <simonenterprise94@gmail.com>
2019-10-15 09:57:30 +0300

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
+import AddProject from './components/Project/AddProject';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Route exact path="/dashboard" component={Dashboard} />
+
+        <Route exact path="/addProject" component={AddProject} />
+
+      </div>
+    </Router>
   );
 }
 
