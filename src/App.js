a012345cea46c23b4c3abbfc50aa065b69a8905a
Simon <simonenterprise94@gmail.com>
2019-10-17 16:16:00 +0300

    feat: wire up redux store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import history from './history';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
+import store from './store';
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-
-        <Route exact path="/dashboard" component={Dashboard} />
-
-        <Route exact path="/addProject" component={AddProject} />
-
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
